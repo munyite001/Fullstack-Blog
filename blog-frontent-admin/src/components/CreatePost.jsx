05e7377c5c0d50140811a5b18b2fe7bb468a45e6
munyite001 <emunyite@gmail.com>
2024-07-01 15:20:00 +0300

    Added publish functionality

--- blog-frontent-admin/src/components/CreatePost.jsx
+++ blog-frontent-admin/src/components/CreatePost.jsx
@@ -14,7 +14,8 @@ export default function CreatePost() {
         title: "",
         content: "",
         tags: [],
-        banner_image: null
+        banner_image: null,
+        published: false
     });
 
     const [mode, setMode] = useState("edit");
@@ -147,6 +148,43 @@ export default function CreatePost() {
         }
     };
 
+    const handlePublish = async () => {
+        if (postId) {
+            try {
+                const token = localStorage.getItem("token");
+                await axios.put(
+                    `http://localhost:3000/api/posts/${postId}`,
+                    { ...postContent, published: true },
+                    {
+                        headers: {
+                            'Authorization': `Bearer ${token}`
+                        }
+                    }
+                );
+                console.log("Post Content Published", postContent)
+                navigate('/posts');
+            } catch (err) {
+                console.log("Error publishing post", err);
+            }
+        } else {
+            try {
+                const token = localStorage.getItem("token")
+                await axios.post(
+                    "http://localhost:3000/api/posts",
+                    {...postContent, published: true},
+                    {
+                        headers: {
+                            'Authorization': `Bearer ${token}`
+                        }
+                    }
+                );
+                navigate('/posts');
+            } catch (err) {
+                console.log("Error publishing post", err);
+            }
+        }
+    }
+
     return (
         <div className="create-post-container">
             <div className="control-btns">
@@ -188,7 +226,7 @@ export default function CreatePost() {
                         <Editor
                             apiKey='lfxzy4rw58a3mnx9kvpzo7px6136mzbk0chc3wm71g760o8w'
                             init={{
-                                plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage advtemplate ai mentions tableofcontents footnotes mergetags autocorrect typography inlinecss markdown',
+                                plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount linkchecker',
                                 toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
                                 ai_request: (request, respondWith) => respondWith.string(() => Promise.reject("See docs to implement AI Assistant")),
                             }}
@@ -199,7 +237,7 @@ export default function CreatePost() {
                     </div>
                     <div className="btns">
                         <button className="btn" onClick={handlePostSave}>Save Draft</button>
-                        <button className="btn">Publish</button>
+                        <button className="btn" onClick={handlePublish}>Publish</button>
                     </div>
                 </div>
             }
